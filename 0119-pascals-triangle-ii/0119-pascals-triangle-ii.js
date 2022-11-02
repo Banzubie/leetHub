a54b0d526f5af3301850187948b6e7b4271b9284
Brian Vose <103958999+Banzubie@users.noreply.github.com>
2022-11-02 12:29:49 -0600

    Time: 100 ms (44.06%), Space: 42.4 MB (23.62%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
@@ -4,13 +4,12 @@
  */
 var getRow = function(rowIndex) {
     var result = [1];
-    var temp = result.slice();
     while (rowIndex > 0) {
         rowIndex--;
+        var temp = result.slice();
         for (var i = 0; i < result.length; i++) {
             result[i] += temp[i - 1] || 0;
         }
-        temp = result.slice();
         result.push(1);
     }
     
