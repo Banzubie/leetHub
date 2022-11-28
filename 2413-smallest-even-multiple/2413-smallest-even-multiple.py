2fc28049d73e80d7541c16501b09d528fb6352d3
Brian Vose <103958999+Banzubie@users.noreply.github.com>
2022-11-27 19:10:06 -0700

    Time: 19 ms (86.26%), Space: 13.3 MB (83.18%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.py
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.py
@@ -1,4 +1,8 @@
-class Solution:
-    def smallestEvenMultiple(self, n: int) -> int:
+class Solution(object):
+    def smallestEvenMultiple(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
         if (n%2==0): return n;
         else: return n*2;
