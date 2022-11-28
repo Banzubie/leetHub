f4f1efb9c795b6caf4743f9c27b301675de1e9ad
Brian Vose <103958999+Banzubie@users.noreply.github.com>
2022-11-27 18:59:16 -0700

    Time: 120 ms (5.21%), Space: 42.4 MB (10.51%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.js
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.js
@@ -6,15 +6,7 @@ var smallestEvenMultiple = function(n) {
     if (n % 2 === 0) {
         return n;
     } else {
-        var t = 2;
-        while (t !== n) {
-            if (t < n) {
-             t += 2;   
-            } else {
-                n += n;
-            }
-        }
-        return n;
+        return n * 2;
     }
     
 };
