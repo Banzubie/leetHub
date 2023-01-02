63c7ed7d055f1fb3b0133268f75f979380ae5054
Brian Vose <103958999+Banzubie@users.noreply.github.com>
2023-01-02 12:11:08 -0700

    Time: 216 ms (45.23%), Space: 47.1 MB (74.92%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+ const map = {M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1}
+var intToRoman = function(num) {
+    var roman = '';
+    for (var key in map) {
+        roman += key.repeat(Math.floor(num / map[key]));
+        num %= map[key];
+    }
+    return roman;
+};
