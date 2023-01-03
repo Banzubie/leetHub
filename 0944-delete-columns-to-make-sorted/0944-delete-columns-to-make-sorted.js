630b96c59705a5b9f6612f07a1009c997745ef90
Brian Vose <103958999+Banzubie@users.noreply.github.com>
2023-01-03 08:34:17 -0700

    Time: 71 ms (91.59%), Space: 46 MB (67.29%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.js
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.js
@@ -3,11 +3,9 @@
  * @return {number}
  */
 var minDeletionSize = function(strs) {
-    const row = strs.length - 1;
-    const col = strs[0].length;
     var delCol = 0;
-    for (let i = 0; i < col; i++) {
-        for (let j = 0; j < row; j++) {
+    for (let i = 0; i < strs[0].length; i++) {
+        for (let j = 0; j < strs.length - 1; j++) {
             if (strs[j][i]>strs[j+1][i]) {
                 delCol++;
                 break;
