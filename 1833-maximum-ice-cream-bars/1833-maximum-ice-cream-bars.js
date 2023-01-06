c84ac921551f41d346029275fc4bd8a3124862e0
Brian Vose <103958999+Banzubie@users.noreply.github.com>
2023-01-06 11:03:26 -0700

    Time: 493 ms (30.00%), Space: 55.6 MB (83.33%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.js
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.js
@@ -6,7 +6,6 @@
 var maxIceCream = function(costs, coins) {
     var bars = costs.sort((a, b) => a-b);
     var boughtBars = 0;
-    console.log(bars)
     for (var i = 0; i < bars.length; i++) {
         if (bars[i] > coins) {
             break;
