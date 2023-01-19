b7803f1ecb43d6eddbdb0ed44eb1773316b295c5
Brian Vose <103958999+Banzubie@users.noreply.github.com>
2023-01-18 22:15:31 -0700

    Time: 60 ms (98.93%), Space: 44.4 MB (42.98%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+var insert = function(intervals, newInterval) {
+    let size = intervals.length;
+    let index = 0;
+    let res = [];
+    
+    while (index < size && intervals[index][1] < newInterval[0]) {
+        res.push(intervals[index]);
+        index++;
+    }
+    while (index < size && intervals[index][0] <= newInterval[1]) {
+        newInterval[0] = Math.min(newInterval[0], intervals[index][0]);
+        newInterval[1] = Math.max(newInterval[1], intervals[index][1]);
+        index++;
+    }
+    res.push(newInterval);
+    while (index < size) {
+        res.push(intervals[index]);
+        index++;
+    }
+    return res;
+};
