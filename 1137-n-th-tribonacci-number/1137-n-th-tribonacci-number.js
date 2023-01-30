e855835f07deba4da935426470270f30945673a4
Brian Vose <103958999+Banzubie@users.noreply.github.com>
2023-01-30 09:07:55 -0700

    Time: 57 ms (93.51%), Space: 42 MB (24.68%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.js
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.js
@@ -4,15 +4,17 @@
  */
 var tribonacci = function(n) {
     if (n <= 1) return n
+    //if (n === 2) return 1
     var total = 0;
     var first = 0;
     var second = 1;
     var third = 1;
-    for (var i = 3; i <= n; i++) {
+    while (n > 2) {
         total = first + second + third
         first = second
         second = third
         third = total
+        n--
     }
     return third;
 };
