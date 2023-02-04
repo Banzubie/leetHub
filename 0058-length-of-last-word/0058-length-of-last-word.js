72f14fb8682b1aaead598694f232f4088654c279
Brian Vose <103958999+Banzubie@users.noreply.github.com>
2023-02-03 18:34:46 -0700

    Time: 65 ms (64.83%), Space: 41.5 MB (92.60%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,13 +3,7 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    var begin = false;
-    var count = 0;
-    for (var i = s.length -1; i >= 0; i--) {
-        if (s[i] !== ' ') begin = true;
-        if (!begin) continue
-        if (s[i] === ' ') break;
-        count++;
-    }
-    return count;
+    let trimmedString = s.trim();
+    
+    return trimmedString.length - trimmedString.lastIndexOf(' ') - 1;
 };
