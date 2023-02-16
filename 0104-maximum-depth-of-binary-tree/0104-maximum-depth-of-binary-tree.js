7bf45e15216d920fe662d5af405b0b245990cca2
Brian Vose <103958999+Banzubie@users.noreply.github.com>
2023-02-16 09:27:01 -0700

    Time: 87 ms (21.00%), Space: 45.4 MB (19.01%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if (!root) return 0;
+    var stack = [[root, 1]];
+    var max = 0;
+    while (stack.length) {
+        let popped = stack.pop();
+        let node = popped[0]
+        let depth = popped[1]
+        if (node.left) {
+            stack.push([node.left, depth + 1])
+        }
+        if (node.right) {
+            stack.push([node.right, depth + 1])
+        }
+        max = Math.max(max, depth);
+    }
+    return max
+};
