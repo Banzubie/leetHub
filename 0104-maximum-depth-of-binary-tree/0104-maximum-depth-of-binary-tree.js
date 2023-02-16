21818d4d0f7cbd27a6ee7592c03727e469efe791
Brian Vose <103958999+Banzubie@users.noreply.github.com>
2023-02-16 09:42:07 -0700

    Time: 65 ms (89.64%), Space: 44.9 MB (45.82%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -12,19 +12,21 @@
  */
 var maxDepth = function(root) {
     if (!root) return 0;
-    var stack = [[root, 1]];
-    var max = 0;
-    while (stack.length) {
-        let popped = stack.pop();
-        let node = popped[0]
-        let depth = popped[1]
-        if (node.left) {
-            stack.push([node.left, depth + 1])
-        }
-        if (node.right) {
-            stack.push([node.right, depth + 1])
-        }
-        max = Math.max(max, depth);
-    }
-    return max
+    return 1 + Math.max(maxDepth(root.right), maxDepth(root.left))
+    // if (!root) return 0;
+    // var stack = [[root, 1]];
+    // var max = 0;
+    // while (stack.length) {
+    //     let popped = stack.pop();
+    //     let node = popped[0]
+    //     let depth = popped[1]
+    //     if (node.left) {
+    //         stack.push([node.left, depth + 1])
+    //     }
+    //     if (node.right) {
+    //         stack.push([node.right, depth + 1])
+    //     }
+    //     max = Math.max(max, depth);
+    // }
+    // return max
 };
