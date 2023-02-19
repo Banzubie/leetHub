222422560afa9f7c5e5dad7a9e46fd07fcd16b5f
Brian Vose <103958999+Banzubie@users.noreply.github.com>
2023-02-18 20:03:57 -0700

    Time: 73 ms (78.72%), Space: 43.5 MB (60.99%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var reverse = function(x) {
-    if (x < 10 && x > -10) return x;
+    //if (x < 10 && x > -10) return x;
     let neg = x < 0;
     x = Math.abs(x)
     let ans = 0;
@@ -12,8 +12,7 @@ var reverse = function(x) {
         x = Math.floor(x / 10)
         ans *= 10 
         ans += digit
-        if (ans > Math.pow(2, 31)) return 0;
     }
-    
+    if (ans > Math.pow(2, 31)) return 0;
     return neg ? ans * -1 : ans;
 };
