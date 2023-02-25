c7d54f0f98996dd98a76e597ff672d368060eaef
Brian Vose <103958999+Banzubie@users.noreply.github.com>
2023-02-25 15:55:19 -0700

    Time: 75 ms (97.03%), Space: 52 MB (28.87%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,25 +3,14 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    var min = prices[0];
-    var max = prices[0];
-    var collection = [];
-    for (var i = 0; i < prices.length; i++) {
-        if (prices[i] < min) {
-            collection.push(max - min);
-            min = prices[i]
-            max = min
-        }
-        if (prices[i] > max) {
-            max = prices[i]
+    var low = prices[0];
+    var profit = 0;
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] < low) {
+            low = prices[i];
+        } else if (prices[i] - low > profit) {
+            profit = prices[i] - low
         }
     }
-    collection.push(max - min);
-    var result = 0;
-    for (var j = 0; j < collection.length; j++) {
-        if (collection[j] > result) {
-            result = collection[j];
-        }
-    }
-    return result;
+    return profit
 };
