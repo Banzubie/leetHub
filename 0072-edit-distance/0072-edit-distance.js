60801a127676801fac84f8bceee8aca680e2b0c4
Brian Vose <103958999+Banzubie@users.noreply.github.com>
2023-02-26 12:37:12 -0700

    Time: 73 ms (98.38%), Space: 46.4 MB (62.53%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.js
+++ 0072-edit-distance/0072-edit-distance.js
@@ -4,23 +4,23 @@
  * @return {number}
  */
 var minDistance = function(word1, word2) {
-    let dp = Array(word1.length+1).fill(null).map(()=>(Array(word2.length+1).fill(0)));
-
-    for (let i=0;i<dp.length;i++) {
-        dp[i][0] = i
+    let prev = [];
+    for (let i = 0; i <= word2.length; i++) {
+        prev.push(i)
     }
+    let cur = new Array(word2.length + 1).fill(0);
 
-    for (let i=0;i<dp[0].length;i++) {
-        dp[0][i] = i
-    }
-    for (let i = 1;i<dp.length;i++) {
-        for (let j=1;j<dp[0].length;j++) {
-            dp[i][j] = Math.min(
-                            dp[i-1][j]+1, // left
-                            dp[i][j-1]+1, // right
-                            dp[i-1][j-1] + (word1[i-1]!=word2[j-1]?1:0) // diagonal
-                        );
+    for (let i = 1; i <= word1.length; i++) {
+        cur[0] = i
+        for (let j = 1; j <= word2.length; j++) {
+            if (word1[i-1] === word2[j-1]) {
+                cur[j] = prev[j-1]
+            } else {
+                cur[j] = Math.min(prev[j-1] + 1, prev[j] + 1, cur[j-1] + 1)
+            }
         }
+        prev = cur
+        cur = new Array(word2.length + 1).fill(0)
     }
-    return dp[dp.length-1][dp[0].length-1];
+    return prev[prev.length - 1]
 };
